7f27c1df89a89038aa376bb4322da6630b807555
RMMusic <r.m.alien.ua@gmail.com>
2015-10-15 20:04:18 +0300

    1

--- progectForm/hed.php
+++ progectForm/hed.php
@@ -1,23 +1,46 @@
 <?php
-class First{
-    function sqlConnect(){
-        return mysqli_connect("localhost", "root", "", "progectFormdb");
+class First
+{
+    protected $_base = '1';
+    protected $_mySqlConnect;
+
+    public function setBase($outSetBase){
+        $this->_base = $outSetBase;
     }
 
-    function sqlQueryInsert($sqlQuery){
-        $connect = sqlConnect();
-        $result = mysqli_query($connect, $sqlQuery);
-        mysqli_close($connect);
+    public function getBase(){
+        return $this->_base;
+    }
+
+    public function sqlConnect(){
+        return $this->_mySqlConnect = mysqli_connect("localhost", "root", "", "progectFormdb");
+    }
+
+    public function mySqlClose(){
+        mysqli_close($this->_mySqlConnect);
+        $this->_mySqlConnect = null;
+    }
+
+    public function sqlQueryInsert($sqlQuery){
+        $result = mysqli_query($this->_mySqlConnect, $sqlQuery);
         return $result;
     }
-    function sqlQuerySelect(){
-        $connect = sqlConnect();
-        $result = mysqli_query($connect, 'SELECT * FROM names');
+
+    public function sqlQuerySelect(){
+        $result = mysqli_query($this->_mySqlConnect, 'SELECT * FROM names');
         $data =  mysqli_fetch_all($result, MYSQLI_ASSOC);
-        mysqli_close($connect);
         return $data;
     }
 }
+
 $a = new First;
-$a->sqlQuerySelect();
-var_dump($a);
+$a->sqlConnect();
+var_dump($a->sqlQuerySelect());
+var_dump($a->sqlQuerySelect());
+var_dump($a->sqlQuerySelect());
+$a->mySqlClose();
+//var_dump($b);
+//echo $a->getBase();
+//$a->setBase("yo");
+//echo $a->getBase();
+
